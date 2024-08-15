ba93cec27090d03d48d6dd5988dfb6282a3d1314
안혜연 <ahy020606@naver.com>
2024-08-15 18:21:49 +0900

    etc: 온보딩 페이지 아이콘 추가

---
+++ footlog/public/icon/index.ts
@@ -0,0 +1,25 @@
+import CampingIcon from './camping.svg';
+import CaveIcon from './cave.svg';
+import HistoricSiteIcon from './historicSite.svg';
+import MountainIcon from './mountain.svg';
+import OceanIcon from './ocean.svg';
+import RiverIcon from './river.svg';
+import TrailIcon from './trail.svg';
+import VolcanoIcon from './volcano.svg';
+import WaterfallIcon from './waterfall.svg';
+import LeftArrowIcon from './leftArrow.svg';
+import CheckIcon from './check.svg';
+
+export {
+  CampingIcon,
+  CaveIcon,
+  HistoricSiteIcon,
+  MountainIcon,
+  OceanIcon,
+  RiverIcon,
+  TrailIcon,
+  VolcanoIcon,
+  WaterfallIcon,
+  LeftArrowIcon,
+  CheckIcon,
+};
--- footlog/src/app/onboarding/page.tsx
+++ footlog/src/app/onboarding/page.tsx
@@ -1,3 +1,18 @@
+'use client';
+import {
+  MountainIcon,
+  OceanIcon,
+  CaveIcon,
+  RiverIcon,
+  TrailIcon,
+  WaterfallIcon,
+  HistoricSiteIcon,
+  VolcanoIcon,
+  CampingIcon,
+  LeftArrowIcon,
+  CheckIcon,
+} from '@public/icon';
+
 export default function page() {
   return <div>onboarding 페이지의 컴포넌트들이 올 자리, 리액트의 pages폴더의 역할</div>;
 }
