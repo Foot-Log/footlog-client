63262b3fc435324b02827591cebc85ababee5b9a
안혜연 <ahy020606@naver.com>
2024-08-15 17:55:47 +0900

    etc: layout 수정

--- footlog/src/app/layout.tsx
+++ footlog/src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
-
 export const metadata: Metadata = {
   title: 'footlog',
   description: 'Korea plogging course recommendation service',
@@ -17,9 +15,8 @@ export const viewport = 'width=device-width, initial-scale=1.0, maximum-scale=1.
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
       <body>
-        <section className="children-container">{children}</section>
+        <section>{children}</section>
       </body>
     </html>
   );
---
+++ footlog/src/components/common/NavBar/NavItem.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+export default function NavItem() {
+  return <div>NavItem</div>;
+}
