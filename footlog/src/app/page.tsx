10589980653d1c04e09d9f9bec13be0fb8b52113
안혜연 <ahy020606@naver.com>
2024-08-16 16:52:22 +0900

    design: 스플래쉬 뷰

--- footlog/src/app/page.tsx
+++ footlog/src/app/page.tsx
@@ -9,15 +9,15 @@ export default function page() {
   useEffect(() => {
     const timer = setTimeout(() => {
       router.push('/login');
-    }, 30000); // 3초 후에 로그인 페이지로 이동
+    }, 3000); // 3초 후에 로그인 페이지로 이동
 
     return () => clearTimeout(timer); // 컴포넌트 언마운트 시 타이머 정리
   }, []);
 
   return (
-    <main className="relative flex h-full w-full flex-col items-center">
+    <main className="relative flex h-full w-full flex-col items-center pt-72">
       <LogoIcon />
-      <p className="fonts-splash">FootLogger</p>
+      <p className="fonts-splash absolute bottom-7">FootLogger</p>
     </main>
   );
 }
