350a89554b499b9add30676d154e4008e1fedfc2
안혜연 <ahy020606@naver.com>
2024-08-17 21:19:55 +0900

    design: 코스 이미지 위에 텍스트와 저장 아이콘 출력

--- footlog/src/components/common/CoursesSlider/CoursesSlider.tsx
+++ footlog/src/components/common/CoursesSlider/CoursesSlider.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { RecommendCoursesDataTypes } from 'types/CommonTypes';
+import { SaveFilledIcon, SaveOutlineIcon } from '@public/icon';
 
 interface CoursesSliderProps {
   courses: RecommendCoursesDataTypes[]; // RecommendCoursesDataTypes 배열, 즉 코스 데이터를 props로 받음
@@ -13,7 +14,7 @@ export default function CoursesSlider(props: CoursesSliderProps) {
     <section className="mt-5 flex gap-3 overflow-x-auto">
       {courses.map((course) => (
         <Link key={course.id} href={`/details/${course.id}`} passHref>
-          <div className="relative flex h-32 w-32 cursor-pointer overflow-hidden rounded-xl">
+          <figure className="relative flex h-32 w-32 cursor-pointer overflow-hidden rounded-xl">
             <div className="absolute inset-0 z-10 h-full w-full rounded-xl bg-[rgba(0,0,0,0.20)]" />
             <Image
               fill
@@ -23,7 +24,12 @@ export default function CoursesSlider(props: CoursesSliderProps) {
               priority
               className="z-0 object-cover" // 이미지가 배경 아래에 있도록 설정
             />
-          </div>
+            <figcaption className="absolute bottom-0 left-0 z-20 pb-2 pl-2">
+              <h2 className="fonts-courseName">{course.title}</h2>
+              <p className="fonts-courseLocation">{course.subtitle}</p>
+            </figcaption>
+            <div className="absolute right-3 top-3">{course.isSaved ? <SaveFilledIcon /> : <SaveOutlineIcon />}</div>
+          </figure>
         </Link>
       ))}
     </section>
