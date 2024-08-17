6d7b889cfbfbf582714e3dd4261815159b261849
안혜연 <ahy020606@naver.com>
2024-08-17 22:15:33 +0900

    design: SearchBar 간격 수정

--- footlog/src/components/common/SearchBar/SearchBar.tsx
+++ footlog/src/components/common/SearchBar/SearchBar.tsx
@@ -2,9 +2,9 @@ import { SearchIcon } from '@public/icon';
 
 export default function SearchBar() {
   return (
-    <section className="bg-gray_3 rounded-searchBar flex h-[47px] w-[307px] items-center justify-center gap-[10.3px] pl-[11px] pr-[5px]">
+    <section className="bg-gray_3 rounded-searchBar flex h-[42px] w-[307px] items-center gap-[10.3px] px-[11px] pr-[5px]">
       <SearchIcon />
-      <input className="bg-transparent" />
+      <input className="w-5/6 bg-transparent" />
     </section>
   );
 }
