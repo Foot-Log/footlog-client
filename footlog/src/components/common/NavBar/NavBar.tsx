73b76f82f3c4816d5e31ee4d2b69fab5e7a12bec
안혜연 <ahy020606@naver.com>
2024-08-19 17:28:06 +0900

    design: navItem 간격 수정

--- footlog/src/components/common/NavBar/NavBar.tsx
+++ footlog/src/components/common/NavBar/NavBar.tsx
@@ -4,7 +4,7 @@ import NavItem from './NavItem';
 
 export default function NavBar() {
   return (
-    <section className="z-100 h-67pxr absolute bottom-0 flex w-full items-center justify-around bg-white shadow-navBar">
+    <section className="z-100 px-60pxr pt-16pxr pb-6pxr absolute bottom-0 flex w-full items-center justify-between bg-white shadow-navBar">
       {navBarIconsData.map((navItem) => (
         <NavItem
           name={navItem.name}
