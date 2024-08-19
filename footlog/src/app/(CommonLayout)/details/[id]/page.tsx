26dc6a6d60316a7e9975e2d26eb23fdeeb78f6d5
안혜연 <ahy020606@naver.com>
2024-08-20 01:35:26 +0900

    design: 공통 컴포넌트 DetailsHeader

--- footlog/src/app/(CommonLayout)/details/[id]/page.tsx
+++ footlog/src/app/(CommonLayout)/details/[id]/page.tsx
@@ -1,8 +1,6 @@
 'use client';
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
-import { LogoIcon } from '@public/icon';
-import { TeamTextIcon } from '@public/icon';
+import { recommendCoursesData } from '@core/recommendCoursesData';
+import DetailsHeader from '@components/details/DetailsHeader';
 
 interface DetailsPageProps {
   params: { id: string };
@@ -11,5 +9,18 @@ interface DetailsPageProps {
 export default function page(props: DetailsPageProps) {
   const { params } = props;
 
-  return <main className="relative flex h-full w-full flex-col"></main>;
+  // params.id를 숫자로 변환 후 추천 코스 데이터에서 해당 코스를 찾기
+  // 나중에 api 연결할 부분
+  const courseId = parseInt(params.id, 10);
+  const course = recommendCoursesData.find((course) => course.id === courseId);
+
+  if (!course) {
+    return <p>코스 정보 없음</p>;
+  }
+
+  return (
+    <main className="relative flex h-full w-full flex-col">
+      <DetailsHeader title={course.title} isSaved={course.isSaved} />
+    </main>
+  );
 }
--- footlog/src/components/details/DetailsHeader.tsx
+++ footlog/src/components/details/DetailsHeader.tsx
@@ -1,10 +1,26 @@
+import { useRouter } from 'next/navigation';
 import { LeftArrowIcon, SaveOutlineGreenIcon, SaveFilledGreenIcon } from '@public/icon';
 
-export default function DetailsHeader() {
+interface DetailsHeaderProps {
+  title: string;
+  isSaved: boolean;
+}
+
+export default function DetailsHeader(props: DetailsHeaderProps) {
+  const { title, isSaved } = props;
+  const router = useRouter();
+
+  function handleBackBtn() {
+    router.push('/home');
+  }
+
   return (
-    <section className="absolute top-0 flex w-full items-center justify-around bg-white">
-      <LeftArrowIcon />
-      <p fonts-recommendTitle>북한산 생태탐방공원</p>
+    <section className="h-68pxr px-24pxr absolute top-0 flex w-full items-center justify-between bg-white">
+      <button type="button" className="cursor-pointer" onClick={() => handleBackBtn()}>
+        <LeftArrowIcon />
+      </button>
+      <p className="fonts-recommendTitle">{title}</p>
+      {isSaved == true ? <SaveFilledGreenIcon /> : <SaveOutlineGreenIcon />}
     </section>
   );
 }
