b7f6c70e19840ff528b90aefd7636657926522bb
안혜연 <ahy020606@naver.com>
2024-08-19 18:49:35 +0900

    etc: 상세 페이지 폰트 추가

--- footlog/src/components/details/DetailsHeader.tsx
+++ footlog/src/components/details/DetailsHeader.tsx
@@ -4,6 +4,7 @@ export default function DetailsHeader() {
   return (
     <section className="absolute top-0 flex w-full items-center justify-around bg-white">
       <LeftArrowIcon />
+      <p fonts-recommendTitle>북한산 생태탐방공원</p>
     </section>
   );
 }
