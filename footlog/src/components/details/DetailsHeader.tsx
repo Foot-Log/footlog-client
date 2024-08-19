3f53569b7051a169674e953c1c052aa3485bfe3f
안혜연 <ahy020606@naver.com>
2024-08-19 18:28:11 +0900

    etc: 상세 페이지 아이콘 추가

---
+++ footlog/src/components/details/DetailsHeader.tsx
@@ -0,0 +1,9 @@
+import { LeftArrowIcon, SaveOutlineGreenIcon, SaveFilledGreenIcon } from '@public/icon';
+
+export default function DetailsHeader() {
+  return (
+    <section className="absolute top-0 flex w-full items-center justify-around bg-white">
+      <LeftArrowIcon />
+    </section>
+  );
+}
