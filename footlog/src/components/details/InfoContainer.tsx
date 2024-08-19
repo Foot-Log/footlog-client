cbbf70ef882d418312c183cb3621fe62f71f14e4
안혜연 <ahy020606@naver.com>
2024-08-20 03:20:18 +0900

    design: 코스 설명 길어질 경우 말줄임과 더보기 버튼 추가

--- footlog/src/components/details/InfoContainer.tsx
+++ footlog/src/components/details/InfoContainer.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { LocationIcon, PriceIcon, ClockIcon, CallIcon, SiteIcon } from '@public/icon';
 
 interface InfoContainerProps {
@@ -11,6 +12,7 @@ interface InfoContainerProps {
 
 export default function InfoContainer(props: InfoContainerProps) {
   const { description, location, price, time, call, site } = props;
+  const [isExpanded, setIsExpanded] = useState(false);
 
   const infoItems = [
     { icon: <LocationIcon />, text: location },
@@ -20,12 +22,34 @@ export default function InfoContainer(props: InfoContainerProps) {
     { icon: <SiteIcon />, text: site },
   ];
 
+  const toggleDescription = () => {
+    setIsExpanded(!isExpanded); // 펼침 상태 토글
+  };
+
   return (
-    <section className="pt-20pxr px-24pxr flex w-full flex-col">
-      <p className="fonts-detailDescription line-clamp-5">{description}</p>
-      <section className="mt-20pxr mb-25pxr gap-12pxr flex flex-col">
+    <section className="flex w-full flex-col px-24pxr pt-20pxr">
+      <p className={`fonts-detailDescription ${!isExpanded ? 'line-clamp-5' : ''}`}>
+        {isExpanded ? description : `${description.slice(0, 173)}`}
+        {!isExpanded && (
+          <span
+            className="fonts-detailDescription cursor-pointer"
+            style={{ color: '#808080' }}
+            onClick={toggleDescription}>
+            {' ...더보기'}
+          </span>
+        )}
+      </p>
+      {isExpanded && (
+        <span
+          className="fonts-detailDescription cursor-pointer"
+          style={{ color: '#808080' }}
+          onClick={toggleDescription}>
+          {' ...접기'}
+        </span>
+      )}
+      <section className="mb-25pxr mt-20pxr flex flex-col gap-12pxr">
         {infoItems.map((item, index) => (
-          <section key={index} className="gap-12pxr flex">
+          <section key={index} className="flex gap-12pxr">
             {item.icon}
             <p className="fonts-detail">{item.text}</p>
           </section>
