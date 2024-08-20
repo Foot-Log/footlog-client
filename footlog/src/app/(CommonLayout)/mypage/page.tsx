ebdd1ffb1a23dd8425e80fc3610677feb67f3a7e
이예지 <oyatpeach@naver.com>
2024-08-20 11:12:54 +0900

    chore : pxr 단위 수정전 작업

--- footlog/src/app/(CommonLayout)/mypage/page.tsx
+++ footlog/src/app/(CommonLayout)/mypage/page.tsx
@@ -1,3 +1,10 @@
 export default function page() {
-  return <div>page</div>;
+  return (
+    <div>
+      <section className="mt-16pxr flex flex-col overflow-y-auto">
+        <div className="text-20pxr leading-26pxr font-bold text-gray_8">마이 페이지</div>
+      </section>
+      <section></section>
+    </div>
+  );
 }
--- footlog/src/components/common/NavBar/NavBar.tsx
+++ footlog/src/components/common/NavBar/NavBar.tsx
@@ -4,7 +4,7 @@ import NavItem from './NavItem';
 
 export default function NavBar() {
   return (
-    <section className="shadow-navBar z-100 absolute bottom-0 flex h-[67px] w-full items-center justify-around bg-white">
+    <section className="z-100 absolute bottom-0 flex h-[67px] w-full items-center justify-around bg-white shadow-navBar">
       {navBarIconsData.map((navItem) => (
         <NavItem
           name={navItem.name}
