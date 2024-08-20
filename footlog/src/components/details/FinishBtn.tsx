852becfec8d0fda8be30facd262b0f32eabb3b95
안혜연 <ahy020606@naver.com>
2024-08-20 19:11:44 +0900

    design: 간격 조정

--- footlog/src/components/details/FinishBtn.tsx
+++ footlog/src/components/details/FinishBtn.tsx
@@ -8,7 +8,7 @@ export default function FinishBtn(props: FinishBtnProps) {
   const { isComplete } = props;
 
   return (
-    <section className="fixed bottom-68pxr bg-white px-24pxr py-20pxr">
+    <section className="fixed bottom-67pxr h-88pxr bg-white px-24pxr py-20pxr">
       <button
         type="button"
         className={`flex h-48pxr w-345pxr items-center justify-center gap-12pxr rounded-xl ${isComplete ? 'not-allowed bg-gray_2' : 'pointer bg-main-green'}`}
