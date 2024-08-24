83a2f7100e72974502512612583e67f6f368a216
안혜연 <ahy020606@naver.com>
2024-08-25 01:38:47 +0900

    etc: 주석 작성

--- footlog/src/app/(CommonLayout)/home/list/[id]/page.tsx
+++ footlog/src/app/(CommonLayout)/home/list/[id]/page.tsx
@@ -3,13 +3,16 @@ import { CourseDetailsDataTypes } from 'types/CommonTypes';
 import BigLocationCard from '@components/common/BigLocationCard';
 import ListHeader from '@components/home/list/ListHeader';
 
+/*
 interface ListPageProps {
-  title: string;
-  course: CourseDetailsDataTypes[];
+  params: { id: string };
 }
+*/
 
-export default function page(props: ListPageProps) {
-  const { title, course } = props;
+export default function page() {
+  //const {params} = props;
+  // 나중에 api 연결하기
+  //const course: CourseDetailsDataTypes = await getMovieDetails(params.id);
 
   return (
     <main className="relative flex h-full w-full flex-col">
