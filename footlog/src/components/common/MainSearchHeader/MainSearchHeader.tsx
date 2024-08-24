7ecd7987b9b50fd48d9d3b62622cb330927dd84b
안혜연 <ahy020606@naver.com>
2024-08-24 15:51:02 +0900

    refactor: SearchHeader에 검색창 포함하게끔 컴포넌트 분리

--- footlog/src/components/common/MainSearchHeader/MainSearchHeader.tsx
+++ footlog/src/components/common/MainSearchHeader/MainSearchHeader.tsx
@@ -1,11 +1,17 @@
-import { HomeLogoIcon } from '@public/icon';
-import SearchBar from '@components/common/SearchBar/SearchBar';
+'use client';
+import Link from 'next/link';
+import { HomeLogoIcon, SearchIcon } from '@public/icon';
 
 export default function MainSearchHeader() {
   return (
     <section className="absolute top-0 flex w-full items-center justify-around gap-12pxr bg-white px-20pxr pb-16pxr pt-9pxr">
       <HomeLogoIcon />
-      <SearchBar />
+      <Link
+        href="/home/search"
+        className="flex h-42pxr w-307pxr items-center gap-10pxr rounded-searchBar bg-gray-3 pl-12pxr pr-5pxr">
+        <SearchIcon />
+        <input className="fonts-searchPlaceholder w-5/6 bg-transparent" placeholder="코스를 검색해 보세요!" />
+      </Link>
     </section>
   );
 }
--- footlog/src/components/home/search/SearchHeader.tsx
+++ footlog/src/components/home/search/SearchHeader.tsx
@@ -1,9 +1,17 @@
+'use client';
+import { useEffect, useRef } from 'react';
 import { useRouter } from 'next/navigation';
-import ClickedSearchBar from './ClickedSearchBar';
-import { LeftArrowIcon } from '@public/icon';
+import { LeftArrowIcon, SearchIcon } from '@public/icon';
 
 export default function SearchHeader() {
   const router = useRouter();
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus(); // 컴포넌트가 렌더링될 때 input에 포커스
+    }
+  }, []); // 빈 배열을 전달하여 컴포넌트가 처음 렌더링될 때만 실행
 
   function handleBackBtn() {
     router.back();
@@ -14,7 +22,14 @@ export default function SearchHeader() {
       <button type="button" className="cursor-pointer" onClick={() => handleBackBtn()}>
         <LeftArrowIcon />
       </button>
-      <ClickedSearchBar />
+      <section className="rounded-clickedSearchBar flex h-42pxr w-317pxr items-center justify-between gap-16pxr border border-gray-8 bg-white pl-20pxr pr-16pxr">
+        <input
+          ref={inputRef}
+          className="fonts-searchPlaceholder w-full bg-transparent"
+          placeholder="코스를 검색해 보세요!"
+        />
+        <SearchIcon />
+      </section>
     </section>
   );
 }
