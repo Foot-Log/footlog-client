b9561d948ad4e62c6ea9bfcddbc612a1537be5c8
안혜연 <ahy020606@naver.com>
2024-08-28 20:53:15 +0900

    feat: AxiosInstance 생성

---
+++ footlog/src/api/api.ts
@@ -0,0 +1,10 @@
+import axios, { AxiosInstance } from 'axios';
+
+const api: AxiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default api;
