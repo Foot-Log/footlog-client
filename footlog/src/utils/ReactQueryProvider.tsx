345153788ff3e58500370c66f732a92db49dd683
안혜연 <ahy020606@naver.com>
2024-08-28 20:14:52 +0900

    feat: react-query 설치

---
+++ footlog/src/utils/ReactQueryProvider.tsx
@@ -0,0 +1,7 @@
+'use client';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+export default function ReactQueryProvider() {
+  return <div>ReactQueryProvider</div>;
+}
