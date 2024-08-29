8829e683218e38d67617830859415e3d56023e75
안혜연 <ahy020606@naver.com>
2024-08-29 11:30:50 +0900

    feat: usePostLogin 훅

--- footlog/src/components/login/LoginBtn.tsx
+++ footlog/src/components/login/LoginBtn.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 export default function LoginBtn() {
-  const KAKAO_REST_API_KEY = process.env.KAKAO_REST_API_KEY;
-  const KAKAO_REDIRECT_URI = process.env.KAKAO_REDIRECT_URI;
+  const KAKAO_REST_API_KEY = process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY;
+  const KAKAO_REDIRECT_URI = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI;
   const KAKAO_LINK = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
 
   function handleLogin() {
---
+++ footlog/src/hooks/login/usePostLogin.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import api from '@api/api';
+import { loginErrorProps, loginResProps } from 'types/login/loginProps';
+import { setToken } from '@utils/token';
+
+export const usePostLogin = () => {
+  const KAKAO_CODE = new URL(window.location.href).searchParams.get('code');
+  const router = useRouter();
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // 로그인 상태를 추적
+
+  useEffect(() => {
+    if (KAKAO_CODE) {
+      api
+        .post(`api/users/kakao/simpleLogin?code=${KAKAO_CODE}`) // api 명세서 보고 바꾸기
+        .then((res) => {
+          const data = res.data as loginResProps; // AxiosResponse의 data를 loginResProps로 단언
+          setToken(data.response.accessToken);
+          setIsLoggedIn(true); // 로그인 성공 시 상태 업데이트
+          console.log('로그인 성공');
+          router.push('/onboarding');
+        })
+        .catch((err) => {
+          const errorData = err.response.data as loginErrorProps; // 에러 응답 데이터 단언
+          if (errorData.success_or_error_code.status === 404) {
+            console.log('404에러');
+            router.push('/login');
+          } else {
+            console.log('로그인 실패');
+            router.push('/splash');
+          }
+        });
+    }
+  }, [KAKAO_CODE, router]);
+};
