717c3c8cfb1a8395a769379c9c02f368fcf6842c
안혜연 <ahy020606@naver.com>
2024-08-29 21:52:12 +0900

    feat: initializeKakao 함수로 Kakao SDK 초기화

--- footlog/src/app/layout.tsx
+++ footlog/src/app/layout.tsx
@@ -8,16 +8,29 @@ export const viewport = 'width=device-width, initial-scale=1.0, maximum-scale=1.
 
 declare global {
   interface Window {
-    kakao: any;
+    Kakao: any;
   }
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  function initializeKakao() {
+    if (window.Kakao && !window.Kakao.isInitialized()) {
+      window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY);
+    }
+  }
+
   return (
     <html lang="en">
       <head>
         <title>FootLog</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content={viewport} />
+        <link rel="preload" href="https://developers.kakao.com/sdk/js/kakao.js" as="script" />
+        <link
+          rel="preload"
+          href={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAOMAP_KEY}&autoload=false&libraries=services`}
+          as="script"
+        />
       </head>
       <body>
         <ReactQueryProviders>
@@ -25,12 +38,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <section className="h-full w-full">{children}</section>
           </RecoilRoot>
         </ReactQueryProviders>
+        <Script src="https://developers.kakao.com/sdk/js/kakao.js" async onLoad={initializeKakao} />
+        <Script
+          type="text/javascript"
+          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAOMAP_KEY}&autoload=false&libraries=services`}
+          async
+          onLoad={initializeKakao}
+        />
       </body>
-      <Script src="https://developers.kakao.com/sdk/js/kakao.js" async />
-      <Script
-        type="text/javascript"
-        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAOMAP_KEY}&autoload=false&libraries=services`}
-      />
     </html>
   );
 }
