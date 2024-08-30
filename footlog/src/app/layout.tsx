575c2243842e91d4ac0d926c36c5cdbea8c9250c
안혜연 <ahy020606@naver.com>
2024-08-30 22:38:04 +0900

    fix: preload 제거

--- footlog/src/app/layout.tsx
+++ footlog/src/app/layout.tsx
@@ -1,5 +1,4 @@
 'use client';
-import { useEffect } from 'react';
 import './globals.css';
 import { RecoilRoot } from 'recoil';
 import Script from 'next/script';
@@ -9,36 +8,17 @@ export const viewport = 'width=device-width, initial-scale=1.0, maximum-scale=1.
 
 declare global {
   interface Window {
-    Kakao: any;
+    kakao: any;
   }
 }
 
-const initializeKakao = () => {
-  if (window.Kakao && !window.Kakao.isInitialized()) {
-    window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY);
-  }
-};
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    // 초기화 시점에 Kakao 객체가 존재하는지 확인
-    if (window.Kakao) {
-      initializeKakao();
-    }
-  }, []);
-
   return (
     <html lang="en">
       <head>
         <title>FootLog</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content={viewport} />
-        <link rel="preload" href="https://developers.kakao.com/sdk/js/kakao.js" as="script" />
-        <link
-          rel="preload"
-          href={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAOMAP_KEY}&autoload=false&libraries=services`}
-          as="script"
-        />
       </head>
       <body>
         <ReactQueryProviders>
@@ -47,21 +27,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           </RecoilRoot>
         </ReactQueryProviders>
         <Script
-          src="https://developers.kakao.com/sdk/js/kakao.js"
-          onLoad={() => {
-            if (window.Kakao) {
-              initializeKakao();
-            }
-          }}
-        />
-        <Script
           type="text/javascript"
           src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAOMAP_KEY}&autoload=false&libraries=services`}
-          onLoad={() => {
-            if (window.Kakao) {
-              initializeKakao();
-            }
-          }}
         />
       </body>
     </html>
