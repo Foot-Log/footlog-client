45a7d14b9f7cbd29fe1e80e0868f88f3412f95b9
안혜연 <ahy020606@naver.com>
2024-09-11 20:35:09 +0900

    feat: getCourseDetails

---
+++ footlog/src/api/details/getCourseDetails.ts
@@ -0,0 +1,23 @@
+import api from 'api/api';
+import { Response } from 'types/common/Response';
+
+export interface CourseDetailDtoDataTypes {
+  course_id: number;
+  name: string;
+  image: string[];
+  summary: string;
+  address: string;
+  tel: string;
+  isSave: boolean;
+}
+
+export interface RequestPathVariable {
+  course_id: number;
+}
+
+export async function getCourseDetails(props: RequestPathVariable): Promise<Response<CourseDetailDtoDataTypes[]>> {
+  const { course_id } = props;
+
+  const { data } = await api.get(`/api/course/detail/${course_id}`);
+  return data;
+}
