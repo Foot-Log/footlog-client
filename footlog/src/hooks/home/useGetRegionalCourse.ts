0c0f1d7fe7cd60a47b31f1c1b50273ec6b2f0249
안혜연 <ahy020606@naver.com>
2024-09-11 18:50:03 +0900

    etc: queryKey 수정

--- footlog/src/hooks/home/useGetRegionalCourse.ts
+++ footlog/src/hooks/home/useGetRegionalCourse.ts
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import { getRegionalCourse } from '@api/home/getRegionalCourse';
 
 const useGetRegionalCourse = (area_name: string) => {
-  const queryKey = ['getRegionalCourse', area_name]; // 쿼리 키 설정
+  const queryKey = ['getRegionalCourse']; // 쿼리 키 설정
   const queryFn = () => getRegionalCourse({ area_name }); // 쿼리 함수 설정
 
   const { data } = useQuery({
