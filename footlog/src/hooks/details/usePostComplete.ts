7dd56ed1c1d39c9a65570bc7d38489b99a4b5476
안혜연 <ahy020606@naver.com>
2024-09-11 22:27:51 +0900

    feat: usePostComplete

---
+++ footlog/src/hooks/details/usePostComplete.ts
@@ -0,0 +1,17 @@
+import { useMutation } from '@tanstack/react-query';
+import { postComplete } from '@api/details/postComplete';
+import { RequestPathVariable } from '@api/details/postComplete';
+
+const usePostComplete = () => {
+  return useMutation({
+    mutationFn: (data: RequestPathVariable) => postComplete(data),
+    onSuccess: (data) => {
+      console.log('완주 전송 성공', data);
+    },
+    onError: (error) => {
+      console.log('완주 전송 실패', error);
+    },
+  });
+};
+
+export default usePostComplete;
