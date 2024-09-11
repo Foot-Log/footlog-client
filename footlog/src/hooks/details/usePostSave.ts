927d3ee4721b2a2dce24a44816509c5204daa49d
안혜연 <ahy020606@naver.com>
2024-09-11 22:36:51 +0900

    feat: usePostSave

---
+++ footlog/src/hooks/details/usePostSave.ts
@@ -0,0 +1,17 @@
+import { useMutation } from '@tanstack/react-query';
+import { postSave } from '@api/details/postSave';
+import { RequestPathVariable } from '@api/details/postSave';
+
+const usePostSave = () => {
+  return useMutation({
+    mutationFn: (data: RequestPathVariable) => postSave(data),
+    onSuccess: (data) => {
+      console.log('저장 전송 성공', data);
+    },
+    onError: (error) => {
+      console.log('저장 전송 실패', error);
+    },
+  });
+};
+
+export default usePostSave;
