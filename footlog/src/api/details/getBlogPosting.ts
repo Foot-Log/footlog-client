f1782f74592863076b8a85ba78b16913e3c0c22b
안혜연 <ahy020606@naver.com>
2024-09-12 15:33:20 +0900

    feat: getBlogPosting

---
+++ footlog/src/api/details/getBlogPosting.ts
@@ -0,0 +1,18 @@
+import api from 'api/api';
+import { Response } from 'types/common/Response';
+import { RequestPathVariable } from 'types/home/details/DetailsTypes';
+
+export interface NaverBlogDtoDataTypes {
+  title: string;
+  link: string;
+  summary: string;
+  blog_name: string;
+  post_date: string;
+}
+
+export async function getBlogPosting(props: RequestPathVariable): Promise<Response<NaverBlogDtoDataTypes>> {
+  const { course_id } = props;
+
+  const { data } = await api.get(`/api/course/post/${course_id}`);
+  return data;
+}
