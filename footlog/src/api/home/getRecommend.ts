00b62db585764ab1a7de145a187f77f0d2cadcd3
안혜연 <ahy020606@naver.com>
2024-09-15 18:22:54 +0900

    feat: CoursesSlider에서 코스 저장 상태 관리

--- footlog/src/api/home/getRecommend.ts
+++
@@ -1,8 +0,0 @@
-import api from 'api/api';
-import { Response } from 'types/common/Response';
-import { CourseResponseDtoDataTypes } from './list/getRegionalCourse';
-
-export async function getRecommend(): Promise<Response<CourseResponseDtoDataTypes[]>> {
-  const { data } = await api.get(`/course/recommend`);
-  return data;
-}
