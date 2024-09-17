80a795aec642f859cf505d4cb9c1b2b040e6d639
안혜연 <ahy020606@naver.com>
2024-09-17 14:35:18 +0900

    feat: 최근 검색어가 없을 경우 안내 텍스트 출력

--- footlog/src/components/home/search/RecentSearchContainer.tsx
+++ footlog/src/components/home/search/RecentSearchContainer.tsx
@@ -12,15 +12,19 @@ export default function RecentSearchContainer(props: RecentSearchContainerProps)
   return (
     <section className="flex w-full flex-col gap-19pxr pl-24pxr pt-12pxr">
       <h2 className="fonts-recommendTitle">최근 검색어</h2>
-      <section className="flex gap-8pxr overflow-x-auto">
-        {recentSearchData.map((search) => (
-          <div
-            key={search.createdAt}
-            className="flex h-36pxr w-auto items-center justify-center gap-12pxr rounded-searchBox border border-gray-2 px-16pxr">
-            <span className="fonts-recommendSubtitle flex-1 whitespace-nowrap">{search.log}</span>
-            <CloseIcon />
-          </div>
-        ))}
+      <section className="flex items-center justify-center gap-8pxr overflow-x-auto">
+        {recentSearchData.length === 0 ? (
+          <p className="fonts-recommendSubtitle flex">최근 검색어가 존재하지 않습니다.</p>
+        ) : (
+          recentSearchData.map((search) => (
+            <div
+              key={search.createdAt}
+              className="flex h-36pxr w-auto items-center justify-center gap-12pxr rounded-searchBox border border-gray-2 px-16pxr">
+              <span className="fonts-recommendSubtitle flex-1 whitespace-nowrap">{search.log}</span>
+              <CloseIcon />
+            </div>
+          ))
+        )}
       </section>
     </section>
   );
