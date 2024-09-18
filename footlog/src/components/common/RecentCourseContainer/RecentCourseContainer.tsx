718298b0ef19e9f63ef079d766de0b8e64f2843d
안혜연 <ahy020606@naver.com>
2024-09-19 01:17:39 +0900

    design: 최근 확인한 코스 간격 수정

--- footlog/src/components/common/RecentCourseContainer/RecentCourseContainer.tsx
+++ footlog/src/components/common/RecentCourseContainer/RecentCourseContainer.tsx
@@ -7,9 +7,9 @@ export default function RecentCourseContainer(props: CoursesDataTypes) {
   return (
     <section className="flex w-full flex-col py-28pxr pl-24pxr">
       <h2 className="fonts-recommendTitle">최근 확인한 코스</h2>
-      <section className="flex items-center justify-center">
+      <section className="flex items-center">
         {courses.length === 0 ? (
-          <p className="fonts-recommendSubtitle flex">최근 확인한 코스가 존재하지 않습니다.</p>
+          <p className="fonts-recommendSubtitle flex pt-20pxr">최근 확인한 코스가 존재하지 않습니다.</p>
         ) : (
           <CoursesSlider courses={courses} />
         )}
