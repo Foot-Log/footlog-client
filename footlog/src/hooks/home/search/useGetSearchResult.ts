5e57c1f5c79f3a5c1696064d28f811bb017cbe4e
안혜연 <ahy020606@naver.com>
2024-09-19 01:23:48 +0900

    feat: 검색에서 빈 문자열이 아닐 때만 쿼리 실행

--- footlog/src/hooks/home/search/useGetSearchResult.ts
+++ footlog/src/hooks/home/search/useGetSearchResult.ts
@@ -3,15 +3,12 @@ import { getSearchResult } from '@api/home/search/getSearchResult';
 
 const useGetSearchResult = (keyword: string) => {
   const queryKey = ['getSearchResult', keyword];
-  //const queryFn = () => getSearchResult({ keyword });
-  const queryFn = () => {
-    console.log(`Keyword for API: ${keyword}`); // 로그 추가
-    return getSearchResult({ keyword });
-  };
+  const queryFn = () => getSearchResult({ keyword });
 
   const { data } = useQuery({
     queryKey,
     queryFn,
+    enabled: keyword.trim() !== '', // keyword가 빈 문자열이 아닐 때만 쿼리 실행
   });
 
   return { data };
