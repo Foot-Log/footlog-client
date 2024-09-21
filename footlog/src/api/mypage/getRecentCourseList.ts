a60598fc02350b98857c0a1a3756d73a3ab06783
안혜연 <ahy020606@naver.com>
2024-09-22 00:08:30 +0900

    feat: 저장 목록 페이지 api 연결

--- footlog/src/api/mypage/getRecentCourseList.ts
+++
@@ -1,8 +0,0 @@
-import api from 'api/api';
-import { Response } from 'types/common/Response';
-import { CourseResponseDtoDataTypes } from 'types/common/CommonTypes';
-
-export async function getRecentCourseList(): Promise<Response<CourseResponseDtoDataTypes[]>> {
-  const { data } = await api.get(`/course/recent`);
-  return data;
-}
