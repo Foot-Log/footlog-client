17f4c0ceb8bfd6a27dd2c8ddf3e474ff972a6e61
안혜연 <ahy020606@naver.com>
2024-09-23 03:40:06 +0900

    feat: 시군구 목록에서 해당 코스 없을 경우 문구 출력

--- footlog/src/app/(CommonLayout)/home/list/small/[id]/page.tsx
+++ footlog/src/app/(CommonLayout)/home/list/small/[id]/page.tsx
@@ -46,16 +46,22 @@ export default function Page() {
     <main className="relative flex h-full w-full flex-col">
       <ListHeader title={area_name} />
       <section className="mt-68pxr flex flex-col gap-24pxr pt-12pxr">
-        {courses.slice(0, visibleCount).map((course: CourseResponseDtoDataTypes) => (
-          <section key={course.course_id}>
-            <BigLocationCard course={course} />
-            <div className="h-8pxr w-full bg-gray-1" />
-          </section>
-        ))}
-        {visibleCount < courses.length && (
-          <div id="load-more" className="flex items-center justify-center">
-            <MoonLoader color="#05cbbe" size={20} speedMultiplier={0.5} />
-          </div>
+        {courses.length === 0 ? ( // 코스가 없는 경우 메시지 출력
+          <p className="fonts-recommendSubtitle mt-289pxr flex justify-center">해당 지역의 코스가 없습니다.</p>
+        ) : (
+          <>
+            {courses.slice(0, visibleCount).map((course: CourseResponseDtoDataTypes) => (
+              <section key={course.course_id}>
+                <BigLocationCard course={course} />
+                <div className="h-8pxr w-full bg-gray-1" />
+              </section>
+            ))}
+            {visibleCount < courses.length && (
+              <div id="load-more" className="flex items-center justify-center">
+                <MoonLoader color="#05cbbe" size={20} speedMultiplier={0.5} />
+              </div>
+            )}
+          </>
         )}
       </section>
     </main>
