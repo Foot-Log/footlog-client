ac16dcd9e3d4752a47c7e12f5d547d9167de795a
안혜연 <ahy020606@naver.com>
2024-10-02 11:38:15 +0900

    etc: meta 이미지 변경

--- footlog/src/app/layout.tsx
+++ footlog/src/app/layout.tsx
@@ -24,7 +24,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <meta name="viewport" content={viewport} />
         <meta property="og:title" content="FootLog" />
         <meta property="og:description" content="건강한 발걸음을 위한 국내 플로깅 코스 추천 및 기록 서비스" />
-        <meta property="og:image" content="https://footlog-bucket.s3.ap-northeast-2.amazonaws.com/9b660251-6Logo.png" />
+        <meta
+          property="og:image"
+          content="https://footlog-bucket.s3.ap-northeast-2.amazonaws.com/e3521c18-9logo2.png"
+        />
         <meta property="og:url" content="https://footlog.site" />
       </head>
       <body>
