a8be084d76294501eecc1a5e630df429421e6990
안혜연 <ahy020606@naver.com>
2024-10-02 15:30:23 +0900

    design: 탈퇴 모달창 반응형 적용

--- footlog/src/components/mypage/ConfirmModal.tsx
+++ footlog/src/components/mypage/ConfirmModal.tsx
@@ -7,8 +7,8 @@ export default function ConfirmModal(props: ConfirmModalProps) {
   const { handleConfirmBtn, handleCloseBtn } = props;
 
   return (
-    <main className="fixed inset-0 z-50 flex bg-black bg-opacity-50">
-      <section className="rounded-confirmModal absolute left-59pxr top-278pxr flex h-202pxr w-276pxr flex-col items-center gap-24pxr bg-white pl-12pxr pt-32pxr">
+    <main className="fixed inset-0 z-50 flex w-full justify-center bg-black bg-opacity-50">
+      <section className="absolute top-278pxr flex h-202pxr w-276pxr flex-col items-center gap-24pxr rounded-confirmModal bg-white pl-12pxr pt-32pxr">
         <section className="flex flex-col gap-8pxr">
           <p className="fonts-recommendTitle">정말로 탈퇴하시겠어요?</p>
           <p className="fonts-newLogContent">영구적으로 계정이 삭제됩니다</p>
