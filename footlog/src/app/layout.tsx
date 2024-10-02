9b395d3a1d02a46c0abf29906c14f894da520853
안혜연 <ahy020606@naver.com>
2024-10-02 15:41:39 +0900

    etc: viewport 객체 수정

--- footlog/src/app/layout.tsx
+++ footlog/src/app/layout.tsx
@@ -1,11 +1,20 @@
 'use client';
 import './globals.css';
 import { RecoilRoot } from 'recoil';
+import type { Viewport } from 'next';
 import Script from 'next/script';
 import ReactQueryProviders from '@utils/ReactQueryProvider';
 import useSetScreenSize from '@hooks/common/useSetScreenSize';
 
-export const viewport = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no';
+//export const viewport = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no';
+export const viewport: Viewport = {
+  initialScale: 1,
+  width: 'device-width',
+  maximumScale: 1,
+  viewportFit: 'cover',
+};
+
+const viewportMetaContent = `width=${viewport.width}, initial-scale=${viewport.initialScale}, maximum-scale=${viewport.maximumScale}, viewport-fit=${viewport.viewportFit}`;
 
 declare global {
   interface Window {
@@ -21,7 +30,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <title>FootLog</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content={viewport} />
+        <meta name="viewport" content={viewportMetaContent} />
         <meta property="og:title" content="FootLog" />
         <meta property="og:description" content="건강한 발걸음을 위한 국내 플로깅 코스 추천 및 기록 서비스" />
         <meta
